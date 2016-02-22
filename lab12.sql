0b49e1217e72bac1632e0d630e925eccf34edb13
Zullybur <mattcasiro@gmail.com>
2016-02-22 14:45:10 -0800

    tested working up to q13

--- lab12.sql
+++ lab12.sql
@@ -3,68 +3,105 @@
 set echo on
 SPOOL /Users/MattCasiro/OneDrive/Camosun/comp155/labs/lab12sub.txt
 SET PAGESIZE 50
+SET LINESIZE 120
 
 /*********************************
  Assignment - Lab 12 Solution
- By <give name>
- Lab Section: ??
+ By Matthew Casiro
+ Lab Section: 1A
 **********************************/
 
 /*** Step 1 Create NEW_STUDENTS ***/
 
-REM SQL command here
+CREATE TABLE new_students (
+    temp_id number(5),
+    prog_id char(4),
+    fname varchar2(25),
+    lname varchar2(25),
+    reg_date date
+    );
+
  
 /*** Step 2 Add columns ***/ 
 
-REM SQL command here
+ALTER TABLE new_students
+ADD created_by varchar2(30) NOT NULL;
+ALTER TABLE new_students
+ADD created_date date DEFAULT SYSDATE;
 
 /*** Step 3 Confirm additions ***/     
 
-REM SQL command here
+DESC NEW_STUDENTS
 
 /*** Step 4 Confirm constraints for NEW_STUDENTS ***/
 
-REM SQL command here
+SELECT constraint_name, constraint_type, search_condition
+FROM user_constraints
+WHERE table_name = 'NEW_STUDENTS';
 
 /*** Step 5 Create PROGRAMMES ***/
 
-REM SQL command here
+CREATE TABLE programmes (
+    prog_id char(4),
+    prog_desc varchar2(30)
+    );
 
 /*** Step 6 Run insert script ***/
 
-REM SQL command here
+@/Users/MattCasiro/OneDrive/Camosun/comp155/labs/lab12inserts.sql
 
 /*** Step 7 SELECT from created tables ***/
 
-REM SQL command here
+SELECT *
+FROM new_students;
+
+SELECT *
+FROM programmes;
  
 /*** Step 8 Alter PROGRAMMES ***/
 
-REM SQL command here
+ALTER TABLE programmes
+ADD CONSTRAINT prog_id_pk
+PRIMARY KEY (prog_id);
 
 /*** Step 9 Alter NEW_STUDENTS ***/
 
-REM SQL command here
+ALTER TABLE new_students
+ADD CONSTRAINT tempid_progid_pk
+PRIMARY KEY (temp_id, prog_id);
 
 /*** Step 10 Add foreign key constraint ***/
 
-REM SQL command here
+ALTER TABLE new_students
+ADD CONSTRAINT prog_id_fk
+FOREIGN KEY (prog_id) REFERENCES programmes (prog_id);
 
 /*** Step 11 Confirm constraints for new tables ***/
 
-REM SQL command here
+SELECT table_name, constraint_name, constraint_type, search_condition
+FROM user_constraints
+WHERE table_name = 'NEW_STUDENTS'
+OR table_name = 'PROGRAMMES'
+ORDER BY table_name;
 
 /*** Step 12 Insert into NEW_STUDENTS ***/
 
-REM SQL command here
+INSERT INTO new_students
+VALUES
+    (9517, 'econ', 'Bill', 'Gates', SYSDATE, 'Matthew Casiro', NULL);
 
 /*** Step 13 Explain error ***/
 
-REM You should get an error when you insert the record in Step 12. Why? 
+--The table "programmes" does not contain a prog_ig 'econ' and so
+--we cannot add this entry in to new_students. Prog_id is a foreign
+--key in new_students referencing programmes which means all values
+--for this column in new_students must exist in programmes.
 
 /*** Step 14 Add date constraint ***/
 
-REM SQL command here
+ALTER TABLE new_students
+ADD CONSTRAINT dates_ck
+CHECK (reg_date > TO_DATE('January 1, 1997', 'Month dd, yyyy'));
 
 /*** Step 15 Test date constraint ***/
 
@@ -76,7 +113,8 @@ REM SQL command here
 
 /*** Step 17 Clean up schema ***/
 
-REM SQL commands here
+DROP TABLE new_students PURGE;
+DROP TABLE programmes PURGE;
 
 /*** Step 18 End ***/
 
