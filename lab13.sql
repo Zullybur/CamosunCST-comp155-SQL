c40423ca584e265c7e371474c5e5fdd303ca5074
Zullybur <mattcasiro@gmail.com>
2016-02-24 17:24:52 -0800

    lab 13 complete

--- lab13.sql
+++ lab13.sql
@@ -19,36 +19,54 @@ FROM employees;
 
 /*** Step 2 Display for Dept 90 ***/
 
-REM SQL command here
+SELECT *
+FROM employees_vw
+WHERE department_id = 90;
 
 /*** Step 3 List Views from Dictionary ***/
 
-REM SQL command here
+SELECT view_name, text
+FROM user_views
+WHERE view_name = 'EMPLOYEES_VW';
 
 /*** Step 4 Create DEPT50 view ***/
 
-REM SQL command here
+CREATE VIEW dept50 AS
+SELECT employee_id AS empno, last_name AS employee, department_id AS deptno
+FROM employees
+WHERE department_id = 50
+WITH CHECK OPTION CONSTRAINT dept_id;
 
 /*** Step 5 Structure & Contents of DEPT50 ***/
 
-REM SQL command here (structure)
-REM SQL command here (contents)
+DESC dept50
+
+SELECT *
+FROM dept50
+WHERE rownum <= 10;
 
 /*** Step 6 Reassign Matos ***/
 
-REM SQL command here
+UPDATE dept50
+SET deptno = 80
+WHERE INITCAP(employee) = 'Matos';
 
 /*** Step 7 Why did you get an error? ***/
 
-REM Explain the error here
+-- The "WITH CHECK OPTION" constraint disallows INSERT, UPDATE, and DELETE commands
+-- that would impact the named column (in this case: deptno)
 
 /*** Step 8 List views from USER_VIEWS ***/
 
-REM SQL command here
+SELECT view_name, text
+FROM user_views
+WHERE view_name = 'EMPLOYEES_VW'
+OR view_name = 'DEPT50';
       
 /*** Step 9 Drop the new views ***/
 
-REM SQL command here
+DROP VIEW employees_vw;
+DROP VIEW dept50;
 
 /*** Step 10 ***/
 
